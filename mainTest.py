edc5f7d8550d4380124d499ed5c0d7ac239446a3
hoidol <qkrghdud0@gmail.com>
2022-12-27 19:19:15 +0900

    init

---
+++ mainTest.py
@@ -0,0 +1,14 @@
+s1 = {}
+s2 = {1}
+list = []
+intValue1= 5
+intValue2 = 0
+strValue = "str"
+
+print(bool(s1)) #집합 {}
+print(bool(s2)) #집합 {1}
+print(bool(list)) #리스트 []
+print(bool(intValue1)) #정수 5
+print(bool(intValue2)) #정수 0
+print(bool(strValue)) #문자열 "str"
+
